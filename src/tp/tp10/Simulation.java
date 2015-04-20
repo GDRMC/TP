76b7edc1779c1d3a3e094675fdc10cc03e52f1ec
GDRMc <gdrdu86@gmail.com>
2015-04-20 10:37:05 +0200

    The story of the damn Sim > Episode 2

--- src/tp/tp10/Simulation.java
+++ src/tp/tp10/Simulation.java
@@ -15,35 +15,53 @@ public class Simulation {
     private ArrayList<Clan>simul = new ArrayList();
     
     Simulation(){ 
-        simuler(20,10);
+        simuler();
     }
     
-    public void simuler(int nbC,int nbG){
-        //permet d'ajouter nb sujets de tests dans le clan de simulation
+    public void simuler(){
+        ajouterGuerriers(20,10);
+        //vieillissement
+        while(!finSim()){
+            for(int i=0;i<simul.size();i++){
+                vieillirEtAfficheClan(simul.get(i));
+            }
+        }
+    }
+    
+    private void ajouterGuerriers(int nbC, int nbG){
         for(int i=0;i<nbC;i++){
             simul.add(new Clan());
             for(int j=0;j<nbG;j++){
                 simul.get(i).ajouter(new Guerrier());
             }
+        }   
+    }
+    
+    private int calcMoyAgeClan(Clan cl){
+        int moy = 0;
+        for(int i=0;i<cl.getList().size();i++){
+            moy+=cl.getList().get(i).getAge();
         }
-        //vieillissement
-        int nbOccur = 0;
-        for(int k=0;k<simul.size();k++){
-            while(simul.get(k).getList().size()>0){
-                System.out.println("=Occurence numéro "+nbOccur+++"=");
-                for(int i=0;i<nbC;i++){ //par clan
-                    int moy = 0;
-                    simul.get(i).vieillir();
-                    simul.get(i).nettoyer();
-                    for(int j=0;j<simul.size();j++){
-                        for(int l=0;l<simul.get(l).getList().size();l++)
-                        moy+=simul.get(i).getList().get(i).getAge();
-                    }
-                    System.out.println("Moyenne d'âge du clan: "+moy/simul.get(i).getList().size());
-                    System.out.println("Clan "+i+" "+simul.get(i).toString());
-                }
+        return moy/cl.getList().size();
+    }
+    
+    private void vieillirEtAfficheClan(Clan cl){
+        for(int i=0;i<cl.getList().size();i++){ //par clan
+            simul.get(i).vieillir();
+            simul.get(i).nettoyer();
+            System.out.println("La moyenne d'âge du clan est de "+calcMoyAgeClan(cl)+" ans");
+            System.out.println("Clan "+i+" "+simul.get(i).toString());
+        }
+    }
+
+    private boolean finSim() {
+        boolean end = true;
+        for(int i=0;i<this.simul.size();i++){
+            if(!this.simul.get(i).estDecime()){
+                end = false;
             }
         }
-        
+        System.out.println(end);
+        return end;
     }
 }
